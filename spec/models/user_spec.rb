bb42da0f81b29a3b1ce60f77998f763a1152300c
Richard Voisey <voiseyr15@hotmail.co.uk>
2011-07-17 19:10:59 +0100

    Deleting microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,7 +61,6 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
 
-
   describe "password validations" do
 
     it "should require a password" do
@@ -131,7 +130,6 @@ describe User do
     end
   end
 
-
   describe "admin attribute" do
 
     before(:each) do
@@ -151,7 +149,6 @@ describe User do
       @user.should be_admin
     end
   end
-
 
   describe "micropost associations" do
 
@@ -173,6 +170,24 @@ describe User do
       @user.destroy
       [@mp1, @mp2].each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status feed" do
+
+      it "should have a feed" do
+        @user.should respond_to(:feed)
+      end
+
+      it "should include the user's microposts" do
+        @user.feed.include?(@mp1).should be_true
+        @user.feed.include?(@mp2).should be_true
+      end
+
+      it "should not include a different user's microposts" do
+        mp3 = Factory(:micropost,
+                      :user => Factory(:user, :email => Factory.next(:email)))
+        @user.feed.include?(mp3).should be_false
       end
     end
   end
